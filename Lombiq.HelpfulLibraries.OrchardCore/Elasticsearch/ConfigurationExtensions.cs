ce75d3ef788470ee7e2dc046f30aaa9e8193e6cc
infofromca <zgofmontreal@gmail.com>
2025-05-07 18:23:32 -0400

    Temp

--- Lombiq.HelpfulLibraries.OrchardCore/Elasticsearch/ConfigurationExtensions.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/Elasticsearch/ConfigurationExtensions.cs
@@ -1,16 +1,16 @@
-using Microsoft.Extensions.Configuration;
-using Nest;
-using OrchardCore.Search.Elasticsearch.Core.Models;
+//using Microsoft.Extensions.Configuration;
+//using Nest;
+//using OrchardCore.Search.Elasticsearch.Core.Models;
 
-namespace OrchardCore.Environment.Shell.Configuration;
+//namespace OrchardCore.Environment.Shell.Configuration;
 
-public static class ConfigurationExtensions
-{
-    public static IElasticClient CreateElasticClient(this IShellConfiguration shellConfiguration)
-    {
-        var configuration = shellConfiguration.GetSection("OrchardCore_Elasticsearch");
-        var elasticConfiguration = configuration.Get<ElasticConnectionOptions>();
+//public static class ConfigurationExtensions
+//{
+//    public static IElasticClient CreateElasticClient(this IShellConfiguration shellConfiguration)
+//    {
+//        var configuration = shellConfiguration.GetSection("OrchardCore_Elasticsearch");
+//        var elasticConfiguration = configuration.Get<ElasticConnectionOptions>();
 
-        return new ElasticClient(elasticConfiguration?.GetConnectionSettings());
-    }
-}
+//        return new ElasticClient(elasticConfiguration?.GetConnectionSettings());
+//    }
+//}
--- Lombiq.HelpfulLibraries.OrchardCore/Elasticsearch/ResponseExtensions.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/Elasticsearch/ResponseExtensions.cs
@@ -1,16 +1,16 @@
-using System;
+//using System;
 
-namespace Nest;
+//namespace Nest;
 
-public static class ResponseExtensions
-{
-    public static T ThrowIfFailed<T>(this T response, string message = null)
-        where T : ResponseBase
-    {
-        if (response.IsValid) return response;
+//public static class ResponseExtensions
+//{
+//    public static T ThrowIfFailed<T>(this T response, string message = null)
+//        where T : ResponseBase
+//    {
+//        if (response.IsValid) return response;
 
-        if (!string.IsNullOrWhiteSpace(message)) message = $" ({message.Trim()})";
-        var error = $"Elasticsearch operation failed{message}. {response.DebugInformation}";
-        throw new InvalidOperationException(error);
-    }
-}
+//        if (!string.IsNullOrWhiteSpace(message)) message = $" ({message.Trim()})";
+//        var error = $"Elasticsearch operation failed{message}. {response.DebugInformation}";
+//        throw new InvalidOperationException(error);
+//    }
+//}
