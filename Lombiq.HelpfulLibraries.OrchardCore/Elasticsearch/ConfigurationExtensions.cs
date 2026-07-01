70f7a079a1f9ec11a99ee0c666787a358b4e8d0b
infofromca <zgofmontreal@gmail.com>
2026-07-01 10:54:06 -0400

    osoe-925

--- Lombiq.HelpfulLibraries.OrchardCore/Elasticsearch/ConfigurationExtensions.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/Elasticsearch/ConfigurationExtensions.cs
@@ -1,14 +1,14 @@
 using Elastic.Clients.Elasticsearch;
-//using Nest;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging.Abstractions;
 using OrchardCore.Elasticsearch.Core.Models;
 using OrchardCore.Elasticsearch.Core.Services;
 using System;
 
-//namespace OrchardCore.Environment.Shell.Configuration;
+namespace OrchardCore.Environment.Shell.Configuration;
 
-//public static class ConfigurationExtensions
-//{
+public static class ConfigurationExtensions
+{
     [Obsolete($"Use {nameof(CreateElasticsearchClient)} instead.")]
     public static ElasticsearchClient CreateElasticClient(this IShellConfiguration shellConfiguration) =>
         shellConfiguration.CreateElasticsearchClient();
@@ -22,9 +22,12 @@ using System;
     public static ElasticsearchClient CreateElasticsearchClient(
         this IShellConfiguration shellConfiguration,
         IElasticsearchClientFactory? factory = null)
+    {
         factory ??= new ElasticsearchClientFactory(NullLogger<ElasticsearchClientFactory>.Instance);
 
         var configuration = shellConfiguration.GetSection(ElasticsearchConnectionOptionsConfigurations.ConfigSectionName);
         var connectionOptions = configuration.Get<ElasticsearchConnectionOptions>();
 
         return connectionOptions == null ? new ElasticsearchClient() : factory.Create(connectionOptions);
+    }
+}
